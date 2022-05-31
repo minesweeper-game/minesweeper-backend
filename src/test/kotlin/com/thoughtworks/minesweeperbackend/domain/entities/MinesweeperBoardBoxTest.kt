43fd97f1d7ec2133ee35a21b026ee471c667e9c1
Yusdanis Feus <yusdanis.feus@thoughtworks.com>
2022-05-31 11:33:25 -0500

    Added test to MinesweeperBoardBox class
    Co-authored-by: Fernando Balladares <fernando.balladares@thoughtworks.com>

---
+++ src/test/kotlin/com/thoughtworks/minesweeperbackend/domain/entities/MinesweeperBoardBoxTest.kt
@@ -0,0 +1,19 @@
+package com.thoughtworks.minesweeperbackend.domain.entities
+
+import com.thoughtworks.minesweeperbackend.domain.value_objects.BoardBoxStatus
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+internal class MinesweeperBoardBoxTest{
+
+    @Test
+    fun `should should create minesweeper board box`(){
+        // when
+        val minesweeperBoardBox = MinesweeperBoardBox()
+
+        // then
+        assertThat(minesweeperBoardBox.status).isEqualTo(BoardBoxStatus.CLOSED)
+        assertThat(minesweeperBoardBox.is_mined).isEqualTo(false)
+    }
+}
