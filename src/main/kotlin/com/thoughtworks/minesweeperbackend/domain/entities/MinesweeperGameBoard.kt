9108485b34f5d4d9610a239be1a38bffa4ca9ef6
Fernando Balladares <fernando.balladares@thoughtworks.com>
2022-05-31 12:54:22 -0500

    Added Board columns & rows tests & implementation
    Co-authored-by: Yusdanis Feus <yusdanis.feus@thoughtworks.com>

--- src/main/kotlin/com/thoughtworks/minesweeperbackend/domain/entities/MinesweeperGameBoard.kt
+++ src/main/kotlin/com/thoughtworks/minesweeperbackend/domain/entities/MinesweeperGameBoard.kt
@@ -3,13 +3,25 @@ package com.thoughtworks.minesweeperbackend.domain.entities
 import com.thoughtworks.minesweeperbackend.domain.value_objects.GameDifficulty
 
 class MinesweeperGameBoard {
+
+
+    val MIN_ROWS_AND_COLS = 3
+    val MAX_ROWS_AND_COLS = 17
+
     val rows: Int
     val cols: Int
     val difficulty: GameDifficulty
+    val boardBoxes: Array<Array<MinesweeperBoardBox>>
 
     constructor(rows: Int, cols: Int, difficulty: GameDifficulty ){
+
+        if (rows < MIN_ROWS_AND_COLS || cols < MIN_ROWS_AND_COLS) throw IllegalArgumentException("The minimum amount of rows and cols to be configured is $MIN_ROWS_AND_COLS")
+        if (rows > MAX_ROWS_AND_COLS || cols > MAX_ROWS_AND_COLS) throw IllegalArgumentException("The maximum amount of rows and cols to be configured is $MAX_ROWS_AND_COLS")
+
         this.rows = rows
         this.cols = cols
         this.difficulty = difficulty
+        this.boardBoxes = Array(rows) { Array (cols) { MinesweeperBoardBox() } }
+
     }
 }
--- src/test/kotlin/com/thoughtworks/minesweeperbackend/domain/entities/MinesweeperBoardBoxTest.kt
+++ src/test/kotlin/com/thoughtworks/minesweeperbackend/domain/entities/MinesweeperBoardBoxTest.kt
@@ -2,7 +2,9 @@ package com.thoughtworks.minesweeperbackend.domain.entities
 
 import com.thoughtworks.minesweeperbackend.domain.value_objects.GameDifficulty
 import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+
 
 internal class MinesweeperBoardBoxTest{
 
@@ -20,5 +22,74 @@ internal class MinesweeperBoardBoxTest{
         assertThat(minesweeperGameBoard.rows).isEqualTo(expectedRows)
         assertThat(minesweeperGameBoard.cols).isEqualTo(expectedCols)
         assertThat(minesweeperGameBoard.difficulty).isEqualTo(expectedGameDifficulty)
+        assertThat(minesweeperGameBoard.boardBoxes).isNotEmpty
+        assertThat(minesweeperGameBoard.boardBoxes.size).isEqualTo(expectedRows)
+        assertThat(minesweeperGameBoard.boardBoxes.first().size).isEqualTo(expectedCols)
     }
+
+    @Test
+    fun `should not create a minesweeper game board when min rows is invalid` (){
+        // given
+        val expectedRows = 2
+        val expectedCols = 7
+        val expectedGameDifficulty = GameDifficulty.EASY
+
+        // when
+        val thrown = assertThrows(IllegalArgumentException::class.java) {
+            MinesweeperGameBoard(expectedRows, expectedCols, expectedGameDifficulty)
+        }
+
+        // then
+        assertEquals("The minimum amount of rows and cols to be configured is 3", thrown.message);
+    }
+    
+    @Test
+    fun `should not create a minesweeper game board when min cols is invalid` (){
+        // given
+        val expectedRows = 7
+        val expectedCols = 2
+        val expectedGameDifficulty = GameDifficulty.EASY
+
+        // when
+        val thrown = assertThrows(IllegalArgumentException::class.java) {
+            MinesweeperGameBoard(expectedRows, expectedCols, expectedGameDifficulty)
+        }
+
+        // then
+        assertEquals("The minimum amount of rows and cols to be configured is 3", thrown.message);
+    }
+
+    @Test
+    fun `should not create a minesweeper game board when max rows is invalid` (){
+        // given
+        val expectedRows = 19
+        val expectedCols = 7
+        val expectedGameDifficulty = GameDifficulty.EASY
+
+        // when
+        val thrown = assertThrows(IllegalArgumentException::class.java) {
+            MinesweeperGameBoard(expectedRows, expectedCols, expectedGameDifficulty)
+        }
+
+        // then
+        assertEquals("The maximum amount of rows and cols to be configured is 17", thrown.message);
+    }
+
+    @Test
+    fun `should not create a minesweeper game board when max cols is invalid` (){
+        // given
+        val expectedRows = 7
+        val expectedCols = 19
+        val expectedGameDifficulty = GameDifficulty.EASY
+
+        // when
+        val thrown = assertThrows(IllegalArgumentException::class.java) {
+            MinesweeperGameBoard(expectedRows, expectedCols, expectedGameDifficulty)
+        }
+
+        // then
+        assertEquals("The maximum amount of rows and cols to be configured is 17", thrown.message);
+    }
+    
+
 }
